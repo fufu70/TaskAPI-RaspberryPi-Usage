5df68f8cbd3e06b6c37b8f713fad02b1cf5b6e75
Christian Micklisch <christian.micklisch@successwithsos.com>
2018-09-03 10:45:14 -0400

    Updated request script

--- scripts/request-task.sh
+++ scripts/request-task.sh
@@ -8,6 +8,7 @@ if [ "$HAS_TASK" = false ]; then
     # request task
     TASK_RESULT=$(curl --data "node_hash_id=${NODE_HASH_ID}" "${TASK_API_URL}/task/request")
     TASK_HASH_ID=$(jq -r '.task_hash_id' <<<"$TASK_RESULT")
+    sed -i '/TASK_HASH_ID/d' $SHELL_HOME/$ENVIRONMENT_FILE_NAME; echo "TASK_HASH_ID=${TASK_HASH_ID}" >> $SHELL_HOME/$ENVIRONMENT_FILE_NAME
 
     if [ $TASK_HASH_ID != null ]; then
         TASK_INSTALL_COMMAND=$(jq -r '.install_command' <<<"$TASK_RESULT")
@@ -19,6 +20,7 @@ if [ "$HAS_TASK" = false ]; then
         eval $TASK_START_COMMAND
         eval $TASK_END_COMMAND
     fi
-
+    
+    sed -i '/TASK_HASH_ID/d' $SHELL_HOME/$ENVIRONMENT_FILE_NAME;
     sed -i '/HAS_TASK/d' $SHELL_HOME/$ENVIRONMENT_FILE_NAME; echo "HAS_TASK=false" >> $SHELL_HOME/$ENVIRONMENT_FILE_NAME
 fi
