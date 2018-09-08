c5ba5c40a3e43f829f7f7ddcecbc5f76834c36f3
Christian Micklisch <christian.micklisch@successwithsos.com>
2018-09-08 16:14:38 -0400

    Updated init command to use apt-get

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 # update the machine
-sudo apt -y --force-yes update
+sudo apt-get -y --force-yes update
 
 # Update and install essential 
 sudo apt-get -y --force-yes install build-essential manpages-dev 
 
 #install vim ... cause why not
-sudo apt -y --force-yes install vim nmap
+sudo apt-get -y --force-yes install vim nmap
 
 #install git for getting necessary packages
-sudo apt -y --force-yes install git 
+sudo apt-get -y --force-yes install git 
 
 #install curl for requesting information from the TaskAPI and JQ to parse the JSON objects
-sudo apt -y --force-yes install curl jq
+sudo apt-get -y --force-yes install curl jq
 
 #install inxi to provide metrics to the task api
-sudo apt -y --force-yes install inxi
+sudo apt-get -y --force-yes install inxi
